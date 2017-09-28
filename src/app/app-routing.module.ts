9877401ccd0368472a857d11975f9a4ee8af0c09
zyuiop <zyuiop@zyuiop.net>
2017-09-29 01:56:14 +0200

    Remove useless imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {CollectionComponent} from "./collection/collection.component";
-import {CallbackComponent} from "./auth/callback.component";
-import {PublicCollectionComponent} from "./collection/public-collection.component";
-import {PickUsernameComponent} from "./auth/pick-username.component";
 import {PageNotFoundComponent} from "./page-not-found.component";
 
 const routes: Routes = [
